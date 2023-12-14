d0fec06eaacee7cb7371186f388baf5fdbd96afd
A3A9N <144899095+A3A9N@users.noreply.github.com>
2023-12-14 14:50:37 +0100

    variabelen

--- 01/app.js
+++ 01/app.js
@@ -2,7 +2,14 @@ class App
 {
     runApplication()
     {
-        console.log("Hello world")
+        let appNaam = ("The App");
+        let versienummer = (0.9);
+        let versiedatum = ("14/12/2023");
+        let autheur = ("Adrian Marin");
+        let copyright = ("A3AN");
+        let distributeur = ("Troy");
+        let darkmode = ("Boolean")
+        console.log(appNaam , versienummer);
     }
 }
 
