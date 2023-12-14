ec01ff5c19a2c40493c11ba6cda0c581747fa8bf
A3A9N <144899095+A3A9N@users.noreply.github.com>
2023-12-14 15:26:40 +0100

    kleuren

--- Opd2/02 canvashuis/app.js
+++ Opd2/02 canvashuis/app.js
@@ -4,7 +4,12 @@ class App
     {
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
+
+        g.fillStyle = "green";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "black";
         g.fillRect(0,0,10,10);
+
         console.log (canvas) 
         console.log ("Hallo world")
     }
