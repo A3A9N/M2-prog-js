6f50d40f19002777e848b19e903bd1521ef91bfe
A3A9N <144899095+A3A9N@users.noreply.github.com>
2023-12-14 15:22:48 +0100

    start canvas

---
+++ Opd2/02 canvashuis/app.js
@@ -0,0 +1,14 @@
+class App
+{
+    runApplication()
+    {
+        let canvas = document.getElementById("canvasId")
+        let g = canvas.getContext("2d");
+        g.fillRect(0,0,10,10);
+        console.log (canvas) 
+        console.log ("Hallo world")
+    }
+}
+
+let app = new App();
+app.runApplication();
