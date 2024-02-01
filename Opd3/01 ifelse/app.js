90bbd39cfeed101c94fde252cbf9d2381ff37643
A3A9N <144899095+A3A9N@users.noreply.github.com>
2024-02-01 12:12:43 +0100

    ifelse

--- Opd3/01 ifelse/app.js
+++ Opd3/01 ifelse/app.js
@@ -1,45 +1,35 @@
-class App
-{
-    runApplication()
-    {
-        console.log("hello world")
-       
+class App {
+    runApplication() {
+        console.log("hello world");
+
         let title = document.getElementById("newstitle");
         console.log(title);
- 
+
         let random = Math.random();
         console.log(random);
-        if(random < 0.2)
-        {
-            title.style.backgroundColor = "#FF0000"
-        }
- 
-        else if(random > 0.75)
-        {
-            title.style.backgroundColor = "blue"
+        if (random < 0.2) {
+            title.style.backgroundColor = getRandomColor();
+        } else if (random > 0.75) {
+            title.style.backgroundColor = "blue";
         }
 
-    let newsitem1 = document.getElementsByClassName("gamenews")[0];
-    let newsitem2 = document.getElementsByClassName("gamenews")[1];
- 
-    if(newsitem1 > 0)
-    {
-        newsitem1.style.backgroundColor = "orange"
-    }
-    else(newsitem1 < 0)
-    {
-        newsitem1.style.backgroundColor = "yellow"
-    }
-    if(newsitem2 > 1)
-    {
-        newsitem2.style.backgroundColor = "blue"
-    }
-    else(newsitem2 < 1)
-    {
-        newsitem2.style.backgroundColor = "pink"
-    }
+        let newsitems = document.getElementsByClassName("gamenews");
+        let newsitem1 = newsitems[0];
+        let newsitem2 = newsitems[1];
+
+        newsitem1.style.backgroundColor = getRandomColor();
+        newsitem2.style.backgroundColor = getRandomColor();
     }
 }
- 
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 let app = new App();
 app.runApplication();
